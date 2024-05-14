29c5034d68124f2abf9c773b43f5c4f87642c50e
Adrian Scheit <adrianscheit@gmail.com>
2024-05-14 22:20:50 +0200

    Improve API

--- api/api.ts
+++ api/api.ts
@@ -14,49 +14,47 @@ export interface RootMenuItem {
     items: MenuItem[];
 }
 
-export interface Field {
-    name: string;
+export interface ParametrizedItem {
+    paramName: string;
     label: string;
-    info?: string;
-    placeholder: string;
-    value: string;
-    type: string;
-    disabled: boolean;
-    attributes: { [name: string]: string };
-    triggerActionOnChange: boolean;
 }
 
-export type OutputDataType = 'string' | 'number' | 'link' | 'imgSrc' | 'imgInline';
+export interface Field extends ParametrizedItem {
+    info?: string;
+    type: string;
+    attributes: { [name: string]: string };
+    triggerRequestOnChange: boolean;
+}
+
+export type OutputDataType = 'string' | 'number' | 'link' | 'imgSrc' | 'action';
 
 /// First Column is always the primary key - not displayed directly
-export interface Data2D {
-    name: string;
-    columns: { type: OutputDataType };
+export interface Data2D extends ParametrizedItem {
+    columns: { label: string; type: OutputDataType; };
     data: (string | number)[][];
     selectable: 'no' | 'single' | 'multiple';
+    triggerRequestOnChange: boolean;
 }
 
-export interface Button {
-    name: string;
-    label: string;
+export interface Button extends ParametrizedItem {
+    imgSrc?: string;
 }
 
 export interface Operations {
     header?: string;
-    text?: { text: string; type?: 'error' | 'warning' | 'info' }[];
-    statuses?: { label: string, value: string };
+    texts?: { text: string; type?: 'error' | 'warning' | 'info' }[];
+    tags?: { label: string, value: string };
     fields?: Field[];
     tables?: Data2D[];
     cards?: Data2D[];
-    buttons?: Button[];
+    actions?: Button[];
     dialog?: Operations;
-    closeDialog?: {};
-    tabs?: Operations[];
 }
 
 export interface RootOperations extends Operations {
     menu?: RootMenuItem;
-    navigate?: string[];
+    path?: string[];
     params?: Params;
-    spinner?: boolean;
+    spinnerForSecounds?: number;
+    refreshIntervalSecounds?: number;
 }
--- api/low-level-api.ts
+++ api/low-level-api.ts
@@ -1,7 +1,6 @@
-export type Params = {[name: string]: (string | number | boolean)[]}
+export type Params = { [name: string]: string | string[] | number | number[] | boolean | boolean[] }
 
 export interface Request {
     path: string[];
-    params: {[name: string]: (string | number | boolean)[]};
-    action: string;
+    params: Params;
 }
